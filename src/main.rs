12831da3b0d4996e3712a52a3b469b0642360f58
Ethan Frey <ethanfrey@users.noreply.github.com>
2023-06-05 22:05:29 +0200

    First demo calling thread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rand::{self, Rng};
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let handle = spawn(|| {
+        let delay = rand::thread_rng().gen_range(10..=2000);
+        sleep(Duration::from_millis(delay));
+        println!("Hello from a thread!");
+        5
+    });
+    println!("Awaiting thread");
+    let res = handle.join().unwrap();
+    println!("Thread returned {}", res);
 }
