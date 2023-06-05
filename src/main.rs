15ce3959be8203dfebaa700f0260d98105895eaf
Ethan Frey <ethanfrey@users.noreply.github.com>
2023-06-05 22:16:49 +0200

    Use mpsc to join

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 use rand::{self, Rng};
+use std::sync::mpsc;
 use std::thread;
 use std::time::{Duration, Instant};
 
 fn main() {
     let start = Instant::now();
-    let handles: Vec<_> = (0..10)
+    let (sender, receiver) = mpsc::channel::<String>();
+
+    let _handles: Vec<_> = (0..10)
         .map(|i| {
             let delay = rand::thread_rng().gen_range(10..=2000);
             let builder = thread::Builder::new().name(format!("Thread-{}", i));
+            let my_sender = sender.clone();
             builder
                 .spawn(move || {
                     println!("Thread {} sleeping for {} ms", i, delay);
                     thread::sleep(Duration::from_millis(delay));
-                    thread::current().name().unwrap().to_string()
+                    let name = thread::current().name().unwrap().to_string();
+                    my_sender.send(name).unwrap();
                 })
                 .unwrap()
         })
         .collect();
 
     println!("Awaiting threads");
-    for h in handles {
-        println!("{} finished", h.join().unwrap());
+    for _ in 0..10 {
+        let name = receiver.recv().unwrap();
+        println!("Received: {}", name);
     }
 
     println!("Time elapsed: {:?}", start.elapsed());
