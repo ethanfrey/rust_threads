82699e44f59a519869d63dd8b4481193bd2344ab
Ethan Frey <ethanfrey@users.noreply.github.com>
2023-06-05 22:09:40 +0200

    Vec of threads

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use rand::{self, Rng};
-use std::thread::{sleep, spawn};
-use std::time::Duration;
+use std::thread;
+use std::time::{Duration, Instant};
 
 fn main() {
-    let handle = spawn(|| {
-        let delay = rand::thread_rng().gen_range(10..=2000);
-        sleep(Duration::from_millis(delay));
-        println!("Hello from a thread!");
-        5
-    });
-    println!("Awaiting thread");
-    let res = handle.join().unwrap();
-    println!("Thread returned {}", res);
+    let start = Instant::now();
+    let handles: Vec<_> = (0..10)
+        .map(|i| {
+            let delay = rand::thread_rng().gen_range(10..=2000);
+            let builder = thread::Builder::new().name(format!("Thread-{}", i));
+            builder
+                .spawn(move || {
+                    println!("Thread {} sleeping for {} ms", i, delay);
+                    thread::sleep(Duration::from_millis(delay));
+                    thread::current().name().unwrap().to_string()
+                })
+                .unwrap()
+        })
+        .collect();
+
+    println!("Awaiting threads");
+    for h in handles {
+        println!("{} finished", h.join().unwrap());
+    }
+
+    println!("Time elapsed: {:?}", start.elapsed());
 }
